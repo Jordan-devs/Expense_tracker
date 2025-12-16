365a2b189defc04bf484534d89a136802ece618a
olamide oduola <prifathil@gmail.com>
2025-12-16 12:25:51 +0100

    fixed some cors issues

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,13 +3,20 @@ import cors from "cors";
 import dotenv from "dotenv";
 import { testConnection } from "./db/connectDB.js";
 import authRoutes from "./routes/auth.routes.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL as string,
+    credentials: true,
+  })
+);
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
